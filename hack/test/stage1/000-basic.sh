09a7d74eb8e8533738d64d7f3ccfc35b75e8613a
Ashley Reese <ashley@victorianfox.com>
2019-01-17 15:53:46 +0100

    Add some EDS tests

--- hack/test/stage1/000-basic.sh
+++ hack/test/stage1/000-basic.sh
@@ -7,12 +7,13 @@ echo "PPS: "
 kubectl get pps --all-namespaces -o wide
 echo "-----"
 kubectl apply -f hack/test/stage1/000-service.yaml
+kubectl apply -f hack/test/stage1/000-endpoints.yaml
 sleep 1 # sometimes takes a second to process and re-fetch endpoints
 
 call_grpc envoy.api.v2.ClusterDiscoveryService/FetchClusters
-test_value "cluster names" resources[].name default-test*test-default-test
+test_value "cluster names" resources[].name default-test*test-default-test-e2e-canary
 test_value "EDS Clusters" resources[].edsClusterConfig.edsConfig.apiConfigSource.grpcServices[0].envoyGrpc.clusterName xds_cluster*xds_cluster
-test_value "EDS Cluster Names" resources[].edsClusterConfig.serviceName default-test*test-default-test
+test_value "EDS Cluster Names" resources[].edsClusterConfig.serviceName default-test*test-default-test-e2e-canary
 test_value "Timeout" resources[0].connectTimeout 5s
 
 call_grpc envoy.api.v2.ListenerDiscoveryService/FetchListeners
@@ -31,13 +32,18 @@ test_value "Total weight" resources[0].filterChains[0].filters[0].config.route_c
 
 # test rollback to the original values to check service re-registration
 kubectl apply -f hack/test/stage1/000-service.yaml
-sleep 1 # sometimes takes a second to process and re-fetch endpoints
 call_grpc envoy.api.v2.ClusterDiscoveryService/FetchClusters
-test_value "cluster names" resources[].name default-test*test-default-test
+test_value "cluster names" resources[].name default-test*test-default-test-e2e-canary
 test_value "EDS Clusters" resources[].edsClusterConfig.edsConfig.apiConfigSource.grpcServices[0].envoyGrpc.clusterName xds_cluster*xds_cluster
-test_value "EDS Cluster Names" resources[].edsClusterConfig.serviceName default-test*test-default-test
+test_value "EDS Cluster Names" resources[].edsClusterConfig.serviceName default-test*test-default-test-e2e-canary
 test_value "Timeout" resources[0].connectTimeout "5s"
 
 call_grpc envoy.api.v2.EndpointDiscoveryService/FetchEndpoints
-test_value "EDS discovered port" resources[].endpoints[].lbEndpoints[]?.endpoint.address.socketAddress.portValue 6443
+test_value "EDS discovered port" resources[].endpoints[].lbEndpoints[]?.endpoint.address.socketAddress.portValue 6443*6443*6443*6443
+test_value "EDS discovered addresses" resources[].endpoints[].lbEndpoints[]?.endpoint.address.socketAddress.address 10.1.2.3*10.4.5.6*10.7.8.9*192.168.1.2
+
+kubectl apply -f hack/test/stage1/000-endpoints-updated.yaml
+call_grpc envoy.api.v2.EndpointDiscoveryService/FetchEndpoints
+test_value "EDS discovered port" resources[].endpoints[].lbEndpoints[]?.endpoint.address.socketAddress.portValue 9999*9999*9999*9999
+test_value "EDS discovered addresses" resources[].endpoints[].lbEndpoints[]?.endpoint.address.socketAddress.address 10.10.20.30*10.40.50.60*10.70.80.90*192.168.10.20
 exit 0
